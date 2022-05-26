8ef09624432b2fa9bbd1a6e7035099f0effa7034
SarahWiedrich <sknapcik@gmail.com>
2022-05-25 21:18:20 -0500

    models index.js file created and controllers updated

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,7 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>Rest-Rant</title>
                 <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'/>
                 <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js'/>
                 <link rel='stylesheet' href='/css/style.css'/>

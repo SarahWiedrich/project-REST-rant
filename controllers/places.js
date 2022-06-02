d12c3664799a7660accdcd3c14aad3cb251dc59d
SarahWiedrich <sknapcik@gmail.com>
2022-06-01 21:16:33 -0500

    form field added. show page updated. seed-comments created

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,15 @@
 const router = require ('express').Router()
 const places = require('../models/places.js')
 const db = require('../models')
+const { acceptsEncodings } = require('express/lib/request')
 
-////???
+//Create Routes
+//Create one GET
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
 
+//GET /places
 router.get('/', (req, res) => {
   db.Place.find()
   .then((places) => {
@@ -15,7 +21,24 @@ router.get('/', (req, res) => {
   })
 })
 
+//Read One by id
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id).populate('comments')
+  .then(place => {
+    console.log(place.comments)
+    res.render('places/show', { place })
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
+})
+
+//POST /places
 router.post('/', (req, res) => {
+  if(!req.body.pic){
+    delete req.body['pic']
+  }
   db.Place.create(req.body)
   .then( () => {
     res.redirect('/places')
@@ -36,39 +59,64 @@ router.post('/', (req, res) => {
   })
 })
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
-  .then(place => {
-    res.render('places/show', { place })
-  })
-  .catch(err => {
-    console.log('err', err)
-    res.render('error404')
-  })
-})
-
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
-
+//Delete Place
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  let id = Number(req.params.id)
+  if(isNaN(id)){
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else{
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
 })
 
+//Edit Place
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else {
+    res.render('places/edot', { place: places[id], id })
+  }
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.put('/:id', (req,res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else {
+    if(!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if(!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if(!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
+  }
 })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+router.post('/:id/comment', (req, res) => {
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+    .then(place => {
+      place.comments.push(comment.id)
+      place.save()
+      .then(() => {
+        res.redirect(`/places'${req.params.id}`)
+      })
+    })
+    .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+    })
 })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+        if (data.place.comments.length) {
+            comments = data.place.comments.map(c => {
+                return (
+                    <div className="border">
+                        <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                        <h4>{c.content}</h4>
+                        <h3>
+                            <strong>- {c.author}</strong>
+                        </h3>
+                        <h4>Rating: {c.stars}</h4>
+                    </div>
+                )
+            })
+        }
     return (
         <Def>
           <main>
@@ -26,6 +45,25 @@ function show (data) {
                     <p>No comments yet!</p>
                 </div>
             </div>
+            <form method='POST' action={`/places/${data.place._id}/comment`}>
+                    <div className='form-group'>
+                        <label htmlFor='author'>Author</label>
+                        <input className='form-control' id='author' name='author' type='text'/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='content'>Content</label>
+                        <input className='form-control' type='text' id='content' name='content'/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='stars'>Star Ratig</label>
+                        <input className='form-control' id='stars' name='stars' type='range' min='1' max='5'/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='rant'>Rant?</label>
+                        <input className='form-control' id='rant' name='rant'/>
+                    </div>
+                    <input className='btn btn-primary' type='submit' value='Add comments'/>
+                </form>
             <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
             
             <form method='POST' action={`/places/${data.id}?_method=DELETE`}>

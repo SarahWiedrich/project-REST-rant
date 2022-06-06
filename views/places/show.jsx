a8100e9cc7694cc4012d2f81525c658293f7aa66
SarahWiedrich <sknapcik@gmail.com>
2022-06-06 13:28:52 -0500

    added some css

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -20,7 +20,7 @@ function show (data) {
             let averageRating = Math.round(sumRatings / data.place.comments.length)
             let stars = ''
             for (let i = 0; i < averageRating; i++) {
-                stars += ':star emoji'
+                stars += ''
             }
             rating = (
                 <h3>
@@ -29,7 +29,7 @@ function show (data) {
             )
             comments = data.place.comments.map(c => {
                 return (
-                    <div className="border col-sm-4">
+                    <div className="border">
                         <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
                         <h4>{c.content}</h4>
                         <h3>
@@ -44,7 +44,7 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
-            <div className="card" style={{width: 700}}>
+            <div className="card" style={{width: 500}}>
                 <img className="card-img-top" src={data.place.pic} alt={data.place.name}/>
             <div className="card-body">
             <h5 className="card-title">{data.place.name}</h5>
